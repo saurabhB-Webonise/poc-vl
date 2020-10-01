0dba5345fc94947714a96c288a841dcad7c5a5dc
saurabhB-Webonise <saurabh.bhorkar@weboniselab.com>
2020-10-01 21:47:25 +0530

    UI changes

--- app/src/main/java/com/example/pocvirginialottery/MainActivity.kt
+++ app/src/main/java/com/example/pocvirginialottery/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.pocvirginialottery
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import com.example.pocvirginialottery.retroclient.Api
-import com.example.pocvirginialottery.retroclient.ApiClient
-import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class MainActivity : BaseActivity() {
@@ -26,17 +22,34 @@ class MainActivity : BaseActivity() {
         }
 
         searchButton.setOnClickListener {
+
+            try {
+                val imm: InputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            } catch (e: Exception) {
+
+            }
+
             val zipcode = editZipCode.text.toString()
             if (zipcode.isNotEmpty()) {
                 var intent = Intent(this, MapsActivity::class.java)
                 intent.putExtra("ZIPCODE", zipcode)
                 startActivity(intent)
+
             }
         }
     }
 
-    fun toast(){
-        Toast.makeText(this," Wait! Searching for result...",Toast.LENGTH_LONG).show()
+    override fun onPause() {
+        super.onPause()
+
+
+
+    }
+
+    fun toast() {
+        Toast.makeText(this, " Wait! Searching for result...", Toast.LENGTH_LONG).show()
     }
 
 }
